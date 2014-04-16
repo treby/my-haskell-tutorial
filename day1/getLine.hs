95a73a88b81b02eb6b199bd842103de11119aa63
treby <treby@atelier-nodoka.net>
2014-04-16 23:52:30 +0900

    Where are you from?

--- day1/getLine.hs
+++ day1/getLine.hs
@@ -2,3 +2,6 @@ main = do
   print "What is Your Name?"
   name <- getLine
   print ("hello," ++ name ++ "!")
+  print "Where are you from?"
+  country <- getLine
+  print (name ++ " is from " ++ country)
